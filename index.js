bfe95d67714d12c6e33c402e5210a73e543135cc
Aidan <aidanthesavage10@gmail.com>
2026-07-11 10:56:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -741,6 +741,10 @@ client.on('interactionCreate', async interaction => {
       .setColor('#2b2d31')
       .setThumbnail(interaction.guild.iconURL());
 
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder().setCustomId('lb_levels').setLabel('Level Leaderboard').setStyle(ButtonStyle.Primary),
+    );
+
     return await interaction.reply({ embeds: [lbEmbed], components: [row] });
   }
   
